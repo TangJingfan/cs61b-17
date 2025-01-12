f133c2617eb5c4fc868da5a4d5f4b2a2bbd63f74
tang_jingfan <tangjingfan@gmail.com>
2025-01-12 17:20:34 +0800

    fix: package naming convention

---
+++ code_demo/w1_define_classes/DogLauncher.java
@@ -0,0 +1,9 @@
+import code_demo.w1_define_classes.Dog;
+
+public class DogLauncher {
+  public static void main(String[] args) {
+    Dog d = new Dog();
+    d.weight = 51;
+    d.makeNoise();
+  }
+}
